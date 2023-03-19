137374068da11544d108dbe40af2f61172898da4
Egor <e-okishev@mail.ru>
2023-03-19 11:54:56 +0300

    added fatch & useEffect

--- src/App.js
+++ src/App.js
@@ -4,23 +4,31 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from './assets/pizzas.json'
 
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://641591c88b5d06e4a7b1c2be.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header/>
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/*<Categories/>*/}
+            <Categories/>
             <Sort/>
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((obj) => (
+              items.map((obj) => (
                 <PizzaBlock key={obj.id} {...obj}/>
               ))}
           </div>
